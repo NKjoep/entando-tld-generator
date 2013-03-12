a42c74f6499e06eea777c94b913c67ce5a91bd94
NKjoep <nkjoep@gmail.com>
2013-03-12 19:02:32 +0100

    up

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php echo("we"); ?>
+<html>
+<head>
+	<title>Entando Tld Documentation</title>
+</head>
+<body>
+	<h1>Entando Tld Documentation</h1>
+	
+	<h2>Master</h2>
+	<p>
+		<a href="master/index.html">Browse the latest official version</a>
+	</p>
+	<h2>Develop</h2>
+	<p>
+		<a href="develop/index.html">Browse the unstable version coming from the github develop branch</a>
+	</p>
+</body>
+</html>
